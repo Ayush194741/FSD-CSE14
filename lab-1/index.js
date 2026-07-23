bf9f8af7ace2ca041f7be3b25042eba94b94325e
Ayush Singh <singhneetu7687@gmail.com>
2026-07-23 14:58:29 +0530

    first commit

---
+++ lab-1/index.js
@@ -0,0 +1,17 @@
+function sayHello(name) {
+    console.log(`hello${name}`);
+    }
+
+const sayHi = (name) => {
+    console.log (`hi ${name}`);
+};
+const findSum =(a,b) => {
+    return a+b;
+};
+
+const sum =(a,b) => a+b;
+
+sayHello("Anuj Sharma");
+sayHi("Manju Sinha");
+console.log ("sum=", findSum (5,6));
+console.log ("sum=", sum (5,6));
