cf0e0209196f5690ab3e60fcf628ef05fe2f9e66
Ayush Singh <singhneetu7687@gmail.com>
2026-08-18 10:30:52 +0530

    appjs v1

--- lab-3/app.js
+++ lab-3/app.js
@@ -1,12 +1,9 @@
 import http from 'http';
 
-const server = http.createServer();
-
-server.on('request',(req,res)=>{
-    res.write("<h1>Welcome To Server Side Programming</h1>");
-    res.write("<h1>Nodemon is tracking the files</h1>");
-    res.end();
+const server = http.createServer((req,res)=>{
+    res.end("<h2>Welcome to Server Side</h2>");
 });
+
 
 server.listen(5000,()=>{
     console.log("Server is running");
